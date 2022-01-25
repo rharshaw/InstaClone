6edff760c90555441bf86f89f419d698aed9ae4f
Rian <63377157+rharshaw@users.noreply.github.com>
2022-01-25 17:09:19 -0500

    Initial Commit

---
+++ InstaClone/InstaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCloneApp.swift
+//  InstaClone
+//
+//  Created by Rian Harshaw on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct InstaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
